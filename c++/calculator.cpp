21ed55fd76a1e86b6acb33124a18c514c9c201c0
Caiden <114809995+caidenpy@users.noreply.github.com>
2025-12-02 00:56:14 +0000

    Add files via upload

---
+++ c++/calculator.cpp
@@ -0,0 +1,51 @@
+//calculator project
+
+#include <iostream>
+
+int main(){
+
+    char operation;
+    double  num1;
+    double num2;
+    double result;
+
+    std::cout << "******* CALCULATOR ******* \n"; // start
+
+    std::cout << "Enter either +, -, * or /: ";
+    std::cin >> operation;
+
+    std::cout << "Enter number 1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter number 2: ";
+    std::cin >> num2;
+
+    switch(operation){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0){
+                result = num1 / num2;
+            } else {
+                std::cout << "Error: Division by zero!" << std::endl;
+                return 1;
+            }
+            break;
+        default:
+            std::cout << "Error: Invalid operation!" << std::endl;
+            return 1;
+    }
+
+    std::cout << "Result: " << result << std::endl;
+    std::cout << "******* CALCULATOR *******";  //end 
+
+
+    return 0;
+}
