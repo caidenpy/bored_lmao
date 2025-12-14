a098d131556afe07f64d8295ec89f2dfe47a81e8
Caiden <114809995+caiden3347@users.noreply.github.com>
2025-12-14 19:52:55 +0000

    created new file c++/hello.cpp

---
+++ c++/hello.cpp
@@ -0,0 +1,16 @@
+// this is a simple program that outputs "Hello"
+
+
+#include <iostream>
+
+int main(){
+
+    std::string greeting = "Hello!";
+
+    std::cout << greeting << std::endl;
+
+    return 0;
+}
+
+// end of code
+
